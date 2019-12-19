82d2187eda426eb5c58220c57a7b9cf403bf52a8
Ari Greenberg <agreenberg@lyft.com>
2019-12-18 17:11:52 -0800

    Finish day1

---
+++ adventofcode/day1/day1_2.py
@@ -0,0 +1,26 @@
+# Sum Fuel requirements
+# https://adventofcode.com/2019/day/1
+import fileinput
+
+
+def calc_fuel_requirement_recursively(mass: int) -> int:
+    result = 0
+    while mass > 0:
+        fuel = calc_fuel_requirement(mass)
+        result += fuel
+        mass = fuel
+    return result
+
+
+def calc_fuel_requirement(mass: int) -> int:
+    return max(0, mass // 3 - 2)
+
+
+def main():
+    masses = [int(line) for line in fileinput.input()]
+    fuel_requirements = [calc_fuel_requirement_recursively(mass) for mass in masses]
+    print(sum(fuel_requirements))
+
+
+if __name__ == "__main__":
+    main()

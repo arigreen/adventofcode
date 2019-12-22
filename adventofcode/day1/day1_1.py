5931dd8cb85a1be902f1d8e5d7ae626a8fd14853
Ari Greenberg <agreenberg@lyft.com>
2019-12-21 22:05:00 -0800

    Use template approach for day1_1

--- adventofcode/day1/day1_1.py
+++ adventofcode/day1/day1_1.py
@@ -1,17 +1,42 @@
-# Sum Fuel requirements
-# https://adventofcode.com/2019/day/1
-import fileinput
+import argparse
+
+import pytest
+from support import timing
 
 
 def calc_fuel_requirement(mass: int) -> int:
     return max(0, mass // 3 - 2)
 
 
-def main():
-    masses = [int(line) for line in fileinput.input()]
+def compute(s: str) -> int:
+    masses = [int(line) for line in s.splitlines()]
     fuel_requirements = [calc_fuel_requirement(mass) for mass in masses]
-    print(sum(fuel_requirements))
+    return sum(fuel_requirements)
 
 
-if __name__ == "__main__":
-    main()
+@pytest.mark.parametrize(
+    ('input', 'expected'),
+    (
+        ['12', 2],
+        ['14', 2],
+        ['1969', 654],
+        ['100756', 33583],
+    ),
+)
+def test(input: str, expected: int) -> None:
+    assert compute(input) == expected
+
+
+def main() -> int:
+    parser = argparse.ArgumentParser()
+    parser.add_argument('data_file')
+    args = parser.parse_args()
+
+    with open(args.data_file) as f, timing():
+        print(compute(f.read()))
+
+    return 0
+
+
+if __name__ == '__main__':
+    exit(main())

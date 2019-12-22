850c21780bb6ad56a6789b3531b4096e1744b200
Ari Greenberg <agreenberg@lyft.com>
2019-12-21 22:29:02 -0800

    Add tests to day1 part 2

--- adventofcode/day1/day1_2.py
+++ adventofcode/day1/day1_2.py
@@ -1,6 +1,9 @@
 # Sum Fuel requirements
 # https://adventofcode.com/2019/day/1
-import fileinput
+import argparse
+
+import pytest
+from support import timing
 
 
 def calc_fuel_requirement_recursively(mass: int) -> int:
@@ -16,12 +19,35 @@ def calc_fuel_requirement(mass: int) -> int:
     return max(0, mass // 3 - 2)
 
 
-def main():
-    masses = [int(line) for line in fileinput.input()]
+def compute(s: str) -> int:
+    masses = [int(line) for line in s.splitlines()]
     fuel_requirements = [
         calc_fuel_requirement_recursively(mass) for mass in masses]
-    print(sum(fuel_requirements))
+    return sum(fuel_requirements)
 
 
-if __name__ == "__main__":
-    main()
+@pytest.mark.parametrize(
+    ('input', 'expected'),
+    (
+        ['14', 2],
+        ['1969', 966],
+        ['100756', 50346],
+    ),
+)
+def test(input: str, expected: int) -> None:
+    assert compute(input) == expected
+
+
+def main() -> int:
+    parser = argparse.ArgumentParser()
+    parser.add_argument('data_file')
+    args = parser.parse_args()
+
+    with open(args.data_file) as f, timing():
+        print(compute(f.read()))
+
+    return 0
+
+
+if __name__ == '__main__':
+    exit(main())

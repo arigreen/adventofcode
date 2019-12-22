8c7f38e81749c0ec26d208d41aee40b46d88b008
Ari Greenberg <agreenberg@lyft.com>
2019-12-21 21:39:53 -0800

    Add support and template files

---
+++ adventofcode/template/template.py
@@ -0,0 +1,34 @@
+import argparse
+
+import pytest
+from support import timing
+
+
+def compute(s: str) -> int:
+    # TODO: Implement solution here!
+    return 0
+
+
+@pytest.mark.parametrize(
+    ('input', 'expected'),
+    (
+        # put given test cases here
+    ),
+)
+def test(input_s: str, expected: int) -> None:
+    assert compute(input_s) == expected
+
+
+def main() -> int:
+    parser = argparse.ArgumentParser()
+    parser.add_argument('data_file')
+    args = parser.parse_args()
+
+    with open(args.data_file) as f, timing():
+        print(compute(f.read()))
+
+    return 0
+
+
+if __name__ == '__main__':
+    exit(main())

549276e7a56751ac21c0825fb42319af14f57632
Ari Greenberg <agreenberg@lyft.com>
2019-12-23 12:17:39 -0800

    Implement day3 part 2

--- adventofcode/day3/day3.py
+++ adventofcode/day3/day3.py
@@ -1,8 +1,8 @@
 # Crossed Wires
 # https://adventofcode.com/2019/day/3
 import argparse
+from typing import Dict
 from typing import List
-from typing import Set
 from typing import Tuple
 
 import pytest
@@ -22,29 +22,38 @@ class Wire:
 
     def __init__(self, path: Path):
         self.path = path
-        self.points: Set[Point] = set()
+        self.points_to_steps: Dict[Point, int] = {}
 
     def trace_path(self) -> None:
         current = (0, 0)
+        total_steps = 0
         for step in self.path:
             dir = step[0]
             amount = int(step[1:])
             if dir == 'U':
                 for x in range(amount):
+                    total_steps += 1
                     current = (current[0], current[1]+1)
-                    self.points.add(current)
+                    if current not in self.points_to_steps:
+                        self.points_to_steps[current] = total_steps
             elif dir == 'D':
                 for x in range(amount):
+                    total_steps += 1
                     current = (current[0], current[1]-1)
-                    self.points.add(current)
+                    if current not in self.points_to_steps:
+                        self.points_to_steps[current] = total_steps
             elif dir == 'L':
                 for x in range(amount):
+                    total_steps += 1
                     current = (current[0]-1, current[1])
-                    self.points.add(current)
+                    if current not in self.points_to_steps:
+                        self.points_to_steps[current] = total_steps
             elif dir == 'R':
                 for x in range(amount):
+                    total_steps += 1
                     current = (current[0]+1, current[1])
-                    self.points.add(current)
+                    if current not in self.points_to_steps:
+                        self.points_to_steps[current] = total_steps
 
 
 def parse_data_from_input(input_s: str) -> List[Wire]:
@@ -58,17 +67,6 @@ def manhattan(point: Point) -> int:
     return abs(point[0]) + abs(point[1])
 
 
-def solve_part_1(wire1: Wire, wire2: Wire) -> int:
-    wire1.trace_path()
-    wire2.trace_path()
-    common_points = wire1.points & wire2.points
-    return min(manhattan(point) for point in common_points)
-
-
-def solve_part_2() -> str:
-    return '-1'
-
-
 def compute_1(s: str) -> int:
     wires = parse_data_from_input(s)
     if len(wires) != 2:
@@ -76,13 +74,23 @@ def compute_1(s: str) -> int:
     wire1, wire2 = wires
     wire1.trace_path()
     wire2.trace_path()
-    common_points = wire1.points & wire2.points
+    common_points = wire1.points_to_steps.keys() & wire2.points_to_steps.keys()
     return min(manhattan(point) for point in common_points)
 
 
 def compute_2(s: str) -> int:
     # TODO: Implement solution here!
-    return 0
+    wires = parse_data_from_input(s)
+    if len(wires) != 2:
+        raise Exception("Unexpected input")
+    wire1, wire2 = wires
+    wire1.trace_path()
+    wire2.trace_path()
+    common_points = wire1.points_to_steps.keys() & wire2.points_to_steps.keys()
+    return min(
+        wire1.points_to_steps[point] + wire2.points_to_steps[point]
+        for point in common_points
+    )
 
 
 @pytest.mark.parametrize(
@@ -106,9 +114,18 @@ def test_1(input_s: str, expected: int) -> None:
 
 @pytest.mark.parametrize(
     ('input_s', 'expected'),
-    (
-        # put given test cases here
-    ),
+    [
+        (
+            'R75,D30,R83,U83,L12,D49,R71,U7,L72\n'
+            'U62,R66,U55,R34,D71,R55,D58,R83',
+            610,
+        ),
+        (
+            'R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51\n'
+            'U98,R91,D20,R16,D67,R40,U7,R15,U6,R7',
+            410,
+        ),
+    ],
 )
 def test_2(input_s: str, expected: int) -> None:
     assert compute_2(input_s) == expected

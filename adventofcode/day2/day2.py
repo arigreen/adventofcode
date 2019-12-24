61ac490001ba62230cd89c247614337569d200b7
Ari Greenberg <agreenberg@lyft.com>
2019-12-24 10:06:24 -0800

    Day2 using class template

--- adventofcode/day2/day2.py
+++ adventofcode/day2/day2.py
@@ -1,27 +1,25 @@
-import argparse
 import itertools
 from typing import List
 
 import pytest
-from support import timing
+from AOCProblem import AOCProblem
 
-
-Data = List[int]
+Program = List[int]
 
 GOAL_RESULT = 19690720
 
 
-def execute_command(data: List[int], position: int) -> None:
-    if data[position] == OP_CODES.ADD:
-        input1 = data[data[position+1]]
-        input2 = data[data[position+2]]
-        data[data[position+3]] = input1 + input2
-    elif data[position] == OP_CODES.MULTIPLY:
-        input1 = data[data[position+1]]
-        input2 = data[data[position+2]]
-        data[data[position+3]] = input1 * input2
+def execute_command(program: List[int], position: int) -> None:
+    if program[position] == OP_CODES.ADD:
+        input1 = program[program[position+1]]
+        input2 = program[program[position+2]]
+        program[program[position+3]] = input1 + input2
+    elif program[position] == OP_CODES.MULTIPLY:
+        input1 = program[program[position+1]]
+        input2 = program[program[position+2]]
+        program[program[position+3]] = input1 * input2
     else:
-        raise Exception("Unexpected opcode {}".format(data[position]))
+        raise Exception("Unexpected opcode {}".format(program[position]))
 
 
 class OP_CODES:
@@ -30,52 +28,32 @@ class OP_CODES:
     TERM = 99
 
 
-def should_terminate(data: List[int], position: int) -> bool:
-    return data[position] == OP_CODES.TERM
+def should_terminate(program: List[int], position: int) -> bool:
+    return program[position] == OP_CODES.TERM
 
 
-def parse_data_from_input(input_s: str) -> Data:
+def parse_program_from_input(input_lines: List[str]) -> Program:
     strings = list(itertools.chain.from_iterable(
-        [line.strip().split(",") for line in input_s.splitlines()]))
-    data = [int(x) for x in strings]
-    return data
+        [line.strip().split(",") for line in input_lines]))
+    program = [int(x) for x in strings]
+    return program
 
 
-def set_inputs(data, input1, input2) -> None:
-    data[1] = input1
-    data[2] = input2
+def set_inputs(program, input1, input2) -> None:
+    program[1] = input1
+    program[2] = input2
 
 
-def restore_gravity_assist(data: Data) -> None:
-    set_inputs(data, 12, 2)
+def restore_gravity_assist(program: Program) -> None:
+    set_inputs(program, 12, 2)
 
 
-def execute_program(data: Data) -> int:
+def execute_program(program: Program) -> int:
     position = 0
-    while not should_terminate(data, position):
-        execute_command(data, position)
+    while not should_terminate(program, position):
+        execute_command(program, position)
         position += 4
-    return data[0]
-
-
-def compute_1(s: str) -> int:
-    data = parse_data_from_input(s)
-    restore_gravity_assist(data)
-    return execute_program(data)
-
-
-def compute_2(s: str) -> int:
-    data = parse_data_from_input(s)
-    # Loop through all possible inputs until we find one that matches expected
-    # output
-    for input1 in range(100):
-        for input2 in range(100):
-            data_copy = data[:]
-            set_inputs(data_copy, input1, input2)
-            result = execute_program(data_copy)
-            if result == GOAL_RESULT:
-                return input1 * 100 + input2
-    return 0
+    return program[0]
 
 
 @pytest.mark.parametrize(
@@ -89,7 +67,7 @@ def compute_2(s: str) -> int:
     ),
 )
 def test_1(input_s: str, expected: int) -> None:
-    assert execute_program(parse_data_from_input(input_s)) == expected
+    assert execute_program(parse_program_from_input([input_s])) == expected
 
 
 @pytest.mark.parametrize(
@@ -99,22 +77,29 @@ def test_1(input_s: str, expected: int) -> None:
     ),
 )
 def test_2(input_s: str, expected: int) -> None:
-    assert compute_2(input_s) == expected
+    pass
 
 
-def main() -> int:
-    parser = argparse.ArgumentParser()
-    parser.add_argument('data_file')
-    args = parser.parse_args()
+class Day1(AOCProblem):
 
-    with open(args.data_file) as f, timing():
-        print(f'Part 1: {compute_1(f.read())}')
+    def compute_1(self, input_lines: List[str]) -> int:
+        program = parse_program_from_input(input_lines)
+        restore_gravity_assist(program)
+        return execute_program(program)
 
-    with open(args.data_file) as f, timing():
-        print(f'Part 2: {compute_2(f.read())}')
-
-    return 0
+    def compute_2(self, input_lines: List[str]) -> int:
+        program = parse_program_from_input(input_lines)
+        # Loop through all possible inputs until we find one that matches
+        # expected output
+        for input1 in range(100):
+            for input2 in range(100):
+                program_copy = program[:]
+                set_inputs(program_copy, input1, input2)
+                result = execute_program(program_copy)
+                if result == GOAL_RESULT:
+                    return input1 * 100 + input2
+        return 0
 
 
 if __name__ == '__main__':
-    exit(main())
+    exit(Day1().main())

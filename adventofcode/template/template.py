d5a4c3c38808ba8d706f1a0a81797d6e4a79720a
Ari Greenberg <agreenberg@lyft.com>
2019-12-24 10:14:28 -0800

    update template

--- adventofcode/template/template.py
+++ adventofcode/template/template.py
@@ -1,17 +1,18 @@
-import argparse
+from typing import List
 
 import pytest
-from support import timing
+from AOCProblem import AOCProblem
 
 
-def compute_1(s: str) -> int:
-    # TODO: Implement solution here!
-    return 0
+class DAY_CLASS(AOCProblem):
 
+    def compute_1(self, input_lines: List[str]) -> int:
+        # TODO: Implement solution here!
+        return 0
 
-def compute_2(s: str) -> int:
-    # TODO: Implement solution here!
-    return 0
+    def compute_2(self, input_lines: List[str]) -> int:
+        # TODO: Implement solution here!
+        return 0
 
 
 @pytest.mark.parametrize(
@@ -21,7 +22,7 @@ def compute_2(s: str) -> int:
     ),
 )
 def test_1(input_s: str, expected: int) -> None:
-    assert compute_1(input_s) == expected
+    assert DAY_CLASS().compute_1([input_s]) == expected
 
 
 @pytest.mark.parametrize(
@@ -31,22 +32,8 @@ def test_1(input_s: str, expected: int) -> None:
     ),
 )
 def test_2(input_s: str, expected: int) -> None:
-    assert compute_2(input_s) == expected
-
-
-def main() -> int:
-    parser = argparse.ArgumentParser()
-    parser.add_argument('data_file')
-    args = parser.parse_args()
-
-    with open(args.data_file) as f, timing():
-        print(f'Part 1: {compute_1(f.read())}')
-
-    with open(args.data_file) as f, timing():
-        print(f'Part 2: {compute_2(f.read())}')
-
-    return 0
+    assert DAY_CLASS().compute_1([input_s]) == expected
 
 
 if __name__ == '__main__':
-    exit(main())
+    exit(DAY_CLASS().main())

84464f1de6ab39edaac69da62f36d50fa436ca3f
Ari Greenberg <agreenberg@lyft.com>
2019-12-24 10:25:49 -0800

    Day3 using classes

--- adventofcode/day3/day3.py
+++ adventofcode/day3/day3.py
@@ -1,12 +1,11 @@
 # Crossed Wires
 # https://adventofcode.com/2019/day/3
-import argparse
 from typing import Dict
 from typing import List
 from typing import Tuple
 
 import pytest
-from support import timing
+from AOCProblem import AOCProblem
 
 # General Approach
 # For each wire, maintain a set of points that are in its path
@@ -49,94 +48,77 @@ class Wire:
                     self.points_to_steps[current] = total_steps
 
 
-def parse_data_from_input(input_s: str) -> List[Wire]:
-    lines = input_s.splitlines()
-    if len(lines) != 2:
+def parse_data_from_input(input_lines: List[str]) -> List[Wire]:
+    if len(input_lines) != 2:
         raise Exception("Invalid input")
-    return [Wire(line.strip().split(",")) for line in lines]
+    return [Wire(line.strip().split(",")) for line in input_lines]
 
 
 def manhattan(point: Point) -> int:
     return abs(point[0]) + abs(point[1])
 
 
-def compute_1(s: str) -> int:
-    wires = parse_data_from_input(s)
-    if len(wires) != 2:
-        raise Exception("Unexpected input")
-    wire1, wire2 = wires
-    wire1.trace_path()
-    wire2.trace_path()
-    common_points = wire1.points_to_steps.keys() & wire2.points_to_steps.keys()
-    return min(manhattan(point) for point in common_points)
+class Day3(AOCProblem):
 
+    def compute_1(self, input_lines: List[str]) -> int:
+        wire1, wire2 = parse_data_from_input(input_lines)
+        wire1.trace_path()
+        wire2.trace_path()
+        common_points = (
+            wire1.points_to_steps.keys() & wire2.points_to_steps.keys()
+        )
+        return min(manhattan(point) for point in common_points)
 
-def compute_2(s: str) -> int:
-    # TODO: Implement solution here!
-    wires = parse_data_from_input(s)
-    if len(wires) != 2:
-        raise Exception("Unexpected input")
-    wire1, wire2 = wires
-    wire1.trace_path()
-    wire2.trace_path()
-    common_points = wire1.points_to_steps.keys() & wire2.points_to_steps.keys()
-    return min(
-        wire1.points_to_steps[point] + wire2.points_to_steps[point]
-        for point in common_points
-    )
+    def compute_2(self, input_lines: List[str]) -> int:
+        wire1, wire2 = parse_data_from_input(input_lines)
+        wire1.trace_path()
+        wire2.trace_path()
+        common_points = (
+            wire1.points_to_steps.keys() & wire2.points_to_steps.keys()
+        )
+        return min(
+            wire1.points_to_steps[point] + wire2.points_to_steps[point]
+            for point in common_points
+        )
 
 
 @pytest.mark.parametrize(
-    ('input_s', 'expected'),
+    ('input_lines', 'expected'),
     [
         (
-            'R75,D30,R83,U83,L12,D49,R71,U7,L72\n'
-            'U62,R66,U55,R34,D71,R55,D58,R83',
+            ['R75,D30,R83,U83,L12,D49,R71,U7,L72',
+                'U62,R66,U55,R34,D71,R55,D58,R83'],
             159,
         ),
         (
-            'R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51\n'
-            'U98,R91,D20,R16,D67,R40,U7,R15,U6,R7',
+            ['R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51',
+             'U98,R91,D20,R16,D67,R40,U7,R15,U6,R7'],
             135,
         ),
     ],
 )
-def test_1(input_s: str, expected: int) -> None:
-    assert compute_1(input_s) == expected
+def test_1(input_lines: List[str], expected: int) -> None:
+    assert Day3().compute_1(input_lines) == expected
 
 
 @pytest.mark.parametrize(
-    ('input_s', 'expected'),
+    ('input_lines', 'expected'),
     [
         (
-            'R75,D30,R83,U83,L12,D49,R71,U7,L72\n'
-            'U62,R66,U55,R34,D71,R55,D58,R83',
+            ['R75,D30,R83,U83,L12,D49,R71,U7,L72',
+                'U62,R66,U55,R34,D71,R55,D58,R83'],
             610,
         ),
         (
-            'R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51\n'
-            'U98,R91,D20,R16,D67,R40,U7,R15,U6,R7',
+            ['R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51',
+             'U98,R91,D20,R16,D67,R40,U7,R15,U6,R7'],
             410,
         ),
     ],
 )
-def test_2(input_s: str, expected: int) -> None:
-    assert compute_2(input_s) == expected
-
-
-def main() -> int:
-    parser = argparse.ArgumentParser()
-    parser.add_argument('data_file')
-    args = parser.parse_args()
-
-    with open(args.data_file) as f, timing():
-        print(f'Part 1: {compute_1(f.read())}')
-
-    with open(args.data_file) as f, timing():
-        print(f'Part 2: {compute_2(f.read())}')
-
-    return 0
+def test_2(input_lines: List[str], expected: int) -> None:
+    assert Day3().compute_2(input_lines) == expected
 
 
 if __name__ == '__main__':
-    exit(main())
+    exit(Day3().main())

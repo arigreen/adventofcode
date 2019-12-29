b2d5ac63dc4176bdaa875c3339b737dc8e75fa3c
Ari Greenberg <agreenberg@lyft.com>
2019-12-28 23:06:00 -0800

    Solve Day5 part 2

--- adventofcode/day5/day5.py
+++ adventofcode/day5/day5.py
@@ -16,6 +16,10 @@ class OpCode(Enum):
     MULTIPLY = 2
     SAVE = 3
     OUTPUT = 4
+    JUMP_IF = 5
+    JUMP_IF_NOT = 6
+    LESS_THAN = 7
+    EQUALS = 8
     TERM = 99
 
 
@@ -33,7 +37,7 @@ class Program:
         self.input = value
 
     def read_input(self) -> int:
-        if not self.input:
+        if self.input is None:
             raise Exception("Attemping to read input that does not exist")
         return self.input
 
@@ -85,6 +89,32 @@ class Program:
                 value = self.get_param_value(1, param_modes)
                 self.output(value)
                 self.position += 2
+            elif op_code == OpCode.JUMP_IF:
+                value = self.get_param_value(1, param_modes)
+                if value:
+                    new_position = self.get_param_value(2, param_modes)
+                    self.position = new_position
+                else:
+                    self.position += 3
+            elif op_code == OpCode.JUMP_IF_NOT:
+                value = self.get_param_value(1, param_modes)
+                if not value:
+                    new_position = self.get_param_value(2, param_modes)
+                    self.position = new_position
+                else:
+                    self.position += 3
+            elif op_code == OpCode.LESS_THAN:
+                value1 = self.get_param_value(1, param_modes)
+                value2 = self.get_param_value(2, param_modes)
+                write_position = self.data[self.position + 3]
+                self.data[write_position] = 1 if value1 < value2 else 0
+                self.position += 4
+            elif op_code == OpCode.EQUALS:
+                value1 = self.get_param_value(1, param_modes)
+                value2 = self.get_param_value(2, param_modes)
+                write_position = self.data[self.position + 3]
+                self.data[write_position] = 1 if value1 == value2 else 0
+                self.position += 4
             elif op_code == OpCode.TERM:
                 break
             else:
@@ -120,6 +150,191 @@ def test_full(input_s: str, expected: List[int]) -> None:
     assert program.data == expected
 
 
+@pytest.mark.parametrize(
+    ("data", "input", "expected_output"),
+    (
+        ([3, 9, 8, 9, 10, 9, 4, 9, 99, -1, 8], 1, 0),
+        ([3, 9, 8, 9, 10, 9, 4, 9, 99, -1, 8], 8, 1),
+        ([3, 9, 7, 9, 10, 9, 4, 9, 99, -1, 8], 7, 1),
+        ([3, 9, 7, 9, 10, 9, 4, 9, 99, -1, 8], 8, 0),
+        ([3, 9, 7, 9, 10, 9, 4, 9, 99, -1, 8], 9, 0),
+        ([3, 3, 1108, -1, 8, 3, 4, 3, 99], 4, 0),
+        ([3, 3, 1108, -1, 8, 3, 4, 3, 99], 8, 1),
+        ([3, 3, 1107, -1, 8, 3, 4, 3, 99], 7, 1),
+        ([3, 3, 1107, -1, 8, 3, 4, 3, 99], 8, 0),
+        ([3, 3, 1107, -1, 8, 3, 4, 3, 99], 9, 0),
+        ([3, 12, 6, 12, 15, 1, 13, 14, 13, 4, 13, 99, -1, 0, 1, 9], 0, 0),
+        ([3, 12, 6, 12, 15, 1, 13, 14, 13, 4, 13, 99, -1, 0, 1, 9], 1, 1),
+        ([3, 3, 1105, -1, 9, 1101, 0, 0, 12, 4, 12, 99, 1], 0, 0),
+        ([3, 3, 1105, -1, 9, 1101, 0, 0, 12, 4, 12, 99, 1], 1, 1),
+        (
+            [
+                3,
+                21,
+                1008,
+                21,
+                8,
+                20,
+                1005,
+                20,
+                22,
+                107,
+                8,
+                21,
+                20,
+                1006,
+                20,
+                31,
+                1106,
+                0,
+                36,
+                98,
+                0,
+                0,
+                1002,
+                21,
+                125,
+                20,
+                4,
+                20,
+                1105,
+                1,
+                46,
+                104,
+                999,
+                1105,
+                1,
+                46,
+                1101,
+                1000,
+                1,
+                20,
+                4,
+                20,
+                1105,
+                1,
+                46,
+                98,
+                99,
+            ],
+            7,
+            999,
+        ),
+        (
+            [
+                3,
+                21,
+                1008,
+                21,
+                8,
+                20,
+                1005,
+                20,
+                22,
+                107,
+                8,
+                21,
+                20,
+                1006,
+                20,
+                31,
+                1106,
+                0,
+                36,
+                98,
+                0,
+                0,
+                1002,
+                21,
+                125,
+                20,
+                4,
+                20,
+                1105,
+                1,
+                46,
+                104,
+                999,
+                1105,
+                1,
+                46,
+                1101,
+                1000,
+                1,
+                20,
+                4,
+                20,
+                1105,
+                1,
+                46,
+                98,
+                99,
+            ],
+            8,
+            1000,
+        ),
+        (
+            [
+                3,
+                21,
+                1008,
+                21,
+                8,
+                20,
+                1005,
+                20,
+                22,
+                107,
+                8,
+                21,
+                20,
+                1006,
+                20,
+                31,
+                1106,
+                0,
+                36,
+                98,
+                0,
+                0,
+                1002,
+                21,
+                125,
+                20,
+                4,
+                20,
+                1105,
+                1,
+                46,
+                104,
+                999,
+                1105,
+                1,
+                46,
+                1101,
+                1000,
+                1,
+                20,
+                4,
+                20,
+                1105,
+                1,
+                46,
+                98,
+                99,
+            ],
+            9,
+            1001,
+        ),
+    ),
+)
+def test_with_input_output(data: List[int], input: int, expected_output: int) -> None:
+    program = Program(data)
+    program.set_input(input)
+    program.execute()
+    assert program.outputs == [expected_output]
+
+
 class Day5(AOCProblem):
     def compute_1(self, input_lines: List[str]) -> int:
         program = Program.parse_from_input(input_lines[0])
@@ -131,7 +346,13 @@ class Day5(AOCProblem):
         return outputs[-1]
 
     def compute_2(self, input_lines: List[str]) -> int:
-        return 0
+        program = Program.parse_from_input(input_lines[0])
+        program.set_input(5)
+        program.execute()
+        outputs = program.outputs
+        if any(x for x in outputs[:-1] if x != 0):
+            raise Exception("Function not working correctly")
+        return outputs[-1]
 
 
 if __name__ == "__main__":
